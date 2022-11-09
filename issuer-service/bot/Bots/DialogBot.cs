bcdc33f00d39a6ef9821003c1847b9148f41b157
Tomislav Markovski <tmarkovski@gmail.com>
2022-11-09 15:32:18 -0500

    Issuer bot (#3)
    * Add issuer bot
    
    * Add direct line integration
    
    * Update
    
    * Deploy

---
+++ issuer-service/bot/Bots/DialogBot.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using OkeyDokey.LicenseIssuerBot.Models;
+
+namespace OkeyDokey.LicenseIssuerBot.Bots;
+
+// This IBot implementation can run any type of Dialog. The use of type parameterization is to allows multiple different bots
+// to be run at different endpoints within the same project. This can be achieved by defining distinct Controller types
+// each with dependency on distinct IBot types, this way ASP Dependency Injection can glue everything together without ambiguity.
+// The ConversationState is used by the Dialog system. The UserState isn't, however, it might have been used in a Dialog implementation,
+// and the requirement is that all BotState objects are saved at the end of a turn.
+public class DialogBot<T> : ActivityHandler where T : Dialog
+{
+    protected readonly Dialog Dialog;
+    protected readonly BotState ConversationState;
+    protected readonly BotState UserState;
+    protected readonly ILogger Logger;
+
+    public DialogBot(ConversationState conversationState, UserState userState, T dialog, ILogger<DialogBot<T>> logger)
+    {
+        ConversationState = conversationState;
+        UserState = userState;
+        Dialog = dialog;
+        Logger = logger;
+    }
+
+    protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+    {
+        foreach (ChannelAccount member in membersAdded)
+        {
+            if (member.Id != turnContext.Activity.Recipient.Id)
+            {
+                await turnContext.SendActivityAsync($"Hi there!", cancellationToken: cancellationToken);
+                await turnContext.SendActivityAsync("I'm the Auditor Bot. I can help you get the right farm verification", cancellationToken: cancellationToken);
+
+                await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+            }
+        }
+    }
+
+    public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+    {
+        await base.OnTurnAsync(turnContext, cancellationToken);
+
+        // Save any state changes that might have occurred during the turn.
+        await ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+        await UserState.SaveChangesAsync(turnContext, false, cancellationToken);
+    }
+
+    protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+    {
+        var welcomeUserStateAccessor = UserState.CreateProperty<WelcomeUserState>(nameof(WelcomeUserState));
+        WelcomeUserState didBotWelcomeUser = await welcomeUserStateAccessor.GetAsync(turnContext, () => new WelcomeUserState(), cancellationToken);
+
+        if (didBotWelcomeUser.DidBotWelcomeUser == false)
+        {
+            didBotWelcomeUser.DidBotWelcomeUser = true;
+        }
+
+        Logger.LogInformation("Running dialog with Message Activity.");
+
+        await UserState.SaveChangesAsync(turnContext, cancellationToken: cancellationToken);
+        await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+    }
+}
---
+++ issuer-service/bot/Controllers/DirectLineController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using OkeyDokey.LicenseIssuerBot.Models;
+
+namespace OkeyDokey.LicenseIssuerBot;
+
+[ApiController]
+public class DirectLineController : ControllerBase
+{
+    private readonly DirectLineService _directLineService;
+
+    private readonly string _directLineSecret;
+
+    public DirectLineController(DirectLineService directLineService, IConfiguration configuration)
+    {
+        _directLineService = directLineService;
+        _directLineSecret = configuration["DirectLineSecret"];
+    }
+
+    // Endpoint for generating a Direct Line token bound to a random user ID
+    [HttpGet, HttpPost]
+    [Route("/api/directline/token")]
+    public async Task<IActionResult> Get()
+    {
+        if (!Request.Cookies.TryGetValue("userId", out var randomUserId))
+        {
+            // Generate a random user ID to use for DirectLine token
+            randomUserId = GenerateRandomUserId();
+
+            // Set a cookie, so we can recognize returning user
+            Response.Cookies.Append("userId", randomUserId);
+        }
+
+        DirectLineTokenDetails directLineTokenDetails;
+        try
+        {
+            directLineTokenDetails = await _directLineService.GetTokenAsync(_directLineSecret, randomUserId);
+        }
+        catch (InvalidOperationException invalidOpException)
+        {
+            return BadRequest(new { message = invalidOpException.Message });
+        }
+
+        return Ok(new { token = directLineTokenDetails.Token });
+    }
+
+    // Generates a random user ID
+    // Prefixed with "dl_", as required by the Direct Line API
+    private static string GenerateRandomUserId()
+    {
+        var tokenData = RandomNumberGenerator.GetBytes(16);
+
+        return $"dl_{BitConverter.ToString(tokenData).Replace("-", "").ToLower()}";
+    }
+}
