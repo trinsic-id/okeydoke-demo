63d49f363cb00968fefbf9e7618826543bbacaed
Hersh Patel <hersh.patel@trinsic.id>
2022-11-09 20:42:04 -0500

    Disabling cart for rn

--- src/components/Header/CartButton.tsx
+++ src/components/Header/CartButton.tsx
@@ -4,34 +4,38 @@ import { useRecoilValue } from "recoil";
 import { cartTotalState } from "../../atoms/cart";
 
 export const CartButton = () => {
-  const { totalQty } = useRecoilValue(cartTotalState);
-  return totalQty ? (
-    <Link className="flex flex-row items-center space-x-2" to="cart">
-      <div className="relative">
-        <ShoppingBag size={18} className="stroke-black" />
-        <span
-          className={`absolute -top-2 -right-2 rounded-full bg-blue-500 flex justify-center items-center items ${
-            totalQty === 0 && "opacity-0"
-          }`}
-        >
-          <span className="text-white font-light text-xs px-1">{totalQty}</span>
-        </span>
-      </div>
-      <div className="text-md text-black font-semibold">Cart</div>
-    </Link>
-  ) : (
-    <div className="flex flex-row items-center space-x-2 opacity-30">
-      <div className="relative">
-        <ShoppingBag size={18} className="stroke-black" />
-        <span
-          className={`absolute -top-2 -right-2 rounded-full bg-blue-500 flex justify-center items-center items ${
-            totalQty === 0 && "opacity-0"
-          }`}
-        >
-          <span className="text-white font-light text-xs px-1">{totalQty}</span>
-        </span>
-      </div>
-      <div className="text-md text-black font-semibold">Cart</div>
-    </div>
-  );
+    const { totalQty } = useRecoilValue(cartTotalState);
+    return totalQty ? (
+        <div className="flex flex-row items-center space-x-2">
+            <div className="relative">
+                <ShoppingBag size={18} className="stroke-black" />
+                <span
+                    className={`absolute -top-2 -right-2 rounded-full bg-blue-500 flex justify-center items-center items ${
+                        totalQty === 0 && "opacity-0"
+                    }`}
+                >
+                    <span className="text-white font-light text-xs px-1">
+                        {totalQty}
+                    </span>
+                </span>
+            </div>
+            <div className="text-md text-black font-semibold">Cart</div>
+        </div>
+    ) : (
+        <div className="flex flex-row items-center space-x-2 opacity-30">
+            <div className="relative">
+                <ShoppingBag size={18} className="stroke-black" />
+                <span
+                    className={`absolute -top-2 -right-2 rounded-full bg-blue-500 flex justify-center items-center items ${
+                        totalQty === 0 && "opacity-0"
+                    }`}
+                >
+                    <span className="text-white font-light text-xs px-1">
+                        {totalQty}
+                    </span>
+                </span>
+            </div>
+            <div className="text-md text-black font-semibold">Cart</div>
+        </div>
+    );
 };
