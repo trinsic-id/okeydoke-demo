17b64d24d841f3d080bdfed0bdfddd29af5451ea
Hersh Patel <hersh.patel@trinsic.id>
2022-11-10 15:34:00 -0500

    Adding verified reviews

--- src/layouts/Catalog/Card.tsx
+++ src/layouts/Catalog/Card.tsx
@@ -71,135 +71,166 @@ export const Card = ({
                 animate={Animations.visible}
                 exit={Animations.hidden}
                 key={product.id + "child"}
-                className="flex flex-col items-center gap-3 border p-4 rounded-lg w-full md:max-w-md bg-white hover:shadow-xl"
+                className="flex flex-col items-center rounded-lg w-full md:max-w-md  hover:shadow-xl"
             >
-                <div className="flex flex-row w-full items-center justify-between h-12">
-                    {product.header === ProductHeader.Sale &&
-                        product.discount &&
-                        !isMember && <Sale discount={product.discount} />}
-                    {product.header === ProductHeader.NewSeason &&
-                        !isMember && <NewSeason />}
-                    {memberAdjustment && isGoldMember && <GoldMember />}
-                    {memberAdjustment && isSilverMember && <SilverMember />}
-                    {memberAdjustment && isBronzeMember && <BronzeMember />}
-                    {product.header !== ProductHeader.Sale &&
-                        product.header !== ProductHeader.NewSeason &&
-                        !isGoldMember &&
-                        !isSilverMember &&
-                        !isBronzeMember && <div className="opacity-0">A</div>}
-                    {!isMember && <VerifyCredential />}
-                </div>
-
-                <div className="flex flex-col items-center w-full pt-3 space-y-3 pb-3">
-                    <img
-                        className="w-2/3 max-h-36 rounded-lg"
-                        src={product.image}
-                    />
-                    <div className="text-xl font-medium text-black ">
-                        {product.name}
-                    </div>
-                    <div className="text-md font-light text-gray-500">
-                        {product.subTitle}
+                <div className="flex flex-col items-start w-full gap-3 p-4 rounded-t-lg bg-white">
+                    <div className="flex flex-row w-full items-center justify-between h-12">
+                        {product.header === ProductHeader.Sale &&
+                            product.discount &&
+                            !isMember && <Sale discount={product.discount} />}
+                        {product.header === ProductHeader.NewSeason &&
+                            !isMember && <NewSeason />}
+                        {memberAdjustment && isGoldMember && <GoldMember />}
+                        {memberAdjustment && isSilverMember && <SilverMember />}
+                        {memberAdjustment && isBronzeMember && <BronzeMember />}
+                        {product.header !== ProductHeader.Sale &&
+                            product.header !== ProductHeader.NewSeason &&
+                            !isGoldMember &&
+                            !isSilverMember &&
+                            !isBronzeMember && (
+                                <div className="opacity-0">A</div>
+                            )}
+                        {!isMember && <VerifyCredential />}
                     </div>
 
-                    {!isMember && (
-                        <div className="flex flex-row items-center space-x-4">
-                            <div
-                                className={`text-lg font-medium ${
-                                    product.header === ProductHeader.Sale
-                                        ? "text-red-600"
-                                        : "text-black"
-                                }`}
-                            >
-                                {product.price.toLocaleString("en-US", {
-                                    style: "currency",
-                                    currency: "USD",
-                                })}
-                            </div>
-                            {product.header === ProductHeader.Sale &&
-                                product.prevPrice && (
-                                    <div className="text-md font-light text-gray-500 line-through">
-                                        {product.prevPrice.toLocaleString(
-                                            "en-US",
-                                            {
-                                                style: "currency",
-                                                currency: "USD",
-                                            }
-                                        )}
-                                    </div>
-                                )}
+                    <div className="flex flex-col items-center w-full pt-3 space-y-3 pb-3">
+                        <img
+                            className="w-2/3 max-h-36 rounded-lg"
+                            src={product.image}
+                        />
+                        <div className="text-xl font-medium text-black ">
+                            {product.name}
                         </div>
-                    )}
+                        <div className="text-md font-light text-gray-500">
+                            {product.subTitle}
+                        </div>
 
-                    {isGoldMember && memberAdjustment && (
-                        <div className="flex flex-row items-center space-x-4">
-                            <div className="text-lg font-medium text-yellow-600 ">
-                                {memberAdjustment.goldPrice.toLocaleString(
-                                    "en-US",
-                                    {
+                        {!isMember && (
+                            <div className="flex flex-row items-center space-x-4">
+                                <div
+                                    className={`text-lg font-medium ${
+                                        product.header === ProductHeader.Sale
+                                            ? "text-red-600"
+                                            : "text-black"
+                                    }`}
+                                >
+                                    {product.price.toLocaleString("en-US", {
                                         style: "currency",
                                         currency: "USD",
-                                    }
-                                )}
+                                    })}
+                                </div>
+                                {product.header === ProductHeader.Sale &&
+                                    product.prevPrice && (
+                                        <div className="text-md font-light text-gray-500 line-through">
+                                            {product.prevPrice.toLocaleString(
+                                                "en-US",
+                                                {
+                                                    style: "currency",
+                                                    currency: "USD",
+                                                }
+                                            )}
+                                        </div>
+                                    )}
                             </div>
-                            <div className="text-md font-light text-gray-500 line-through">
-                                {memberAdjustment.prevPrice.toLocaleString(
-                                    "en-US",
-                                    {
-                                        style: "currency",
-                                        currency: "USD",
-                                    }
-                                )}
+                        )}
+
+                        {isGoldMember && memberAdjustment && (
+                            <div className="flex flex-row items-center space-x-4">
+                                <div className="text-lg font-medium text-yellow-600 ">
+                                    {memberAdjustment.goldPrice.toLocaleString(
+                                        "en-US",
+                                        {
+                                            style: "currency",
+                                            currency: "USD",
+                                        }
+                                    )}
+                                </div>
+                                <div className="text-md font-light text-gray-500 line-through">
+                                    {memberAdjustment.prevPrice.toLocaleString(
+                                        "en-US",
+                                        {
+                                            style: "currency",
+                                            currency: "USD",
+                                        }
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    )}
-                    {isSilverMember && memberAdjustment && (
-                        <div className="flex flex-row items-center space-x-4">
-                            <div className="text-lg font-medium text-gray-600 ">
-                                {memberAdjustment.goldPrice.toLocaleString(
-                                    "en-US",
-                                    {
-                                        style: "currency",
-                                        currency: "USD",
-                                    }
-                                )}
+                        )}
+                        {isSilverMember && memberAdjustment && (
+                            <div className="flex flex-row items-center space-x-4">
+                                <div className="text-lg font-medium text-gray-600 ">
+                                    {memberAdjustment.goldPrice.toLocaleString(
+                                        "en-US",
+                                        {
+                                            style: "currency",
+                                            currency: "USD",
+                                        }
+                                    )}
+                                </div>
+                                <div className="text-md font-light text-gray-400 line-through">
+                                    {memberAdjustment.prevPrice.toLocaleString(
+                                        "en-US",
+                                        {
+                                            style: "currency",
+                                            currency: "USD",
+                                        }
+                                    )}
+                                </div>
                             </div>
-                            <div className="text-md font-light text-gray-400 line-through">
-                                {memberAdjustment.prevPrice.toLocaleString(
-                                    "en-US",
-                                    {
-                                        style: "currency",
-                                        currency: "USD",
-                                    }
-                                )}
+                        )}
+                        {isBronzeMember && memberAdjustment && (
+                            <div className="flex flex-row items-center space-x-4">
+                                <div className="text-lg font-medium text-amber-600 ">
+                                    {memberAdjustment.goldPrice.toLocaleString(
+                                        "en-US",
+                                        {
+                                            style: "currency",
+                                            currency: "USD",
+                                        }
+                                    )}
+                                </div>
+                                <div className="text-md font-light text-gray-500 line-through">
+                                    {memberAdjustment.prevPrice.toLocaleString(
+                                        "en-US",
+                                        {
+                                            style: "currency",
+                                            currency: "USD",
+                                        }
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    )}
-                    {isBronzeMember && memberAdjustment && (
-                        <div className="flex flex-row items-center space-x-4">
-                            <div className="text-lg font-medium text-amber-600 ">
-                                {memberAdjustment.goldPrice.toLocaleString(
-                                    "en-US",
-                                    {
-                                        style: "currency",
-                                        currency: "USD",
-                                    }
-                                )}
-                            </div>
-                            <div className="text-md font-light text-gray-500 line-through">
-                                {memberAdjustment.prevPrice.toLocaleString(
-                                    "en-US",
-                                    {
-                                        style: "currency",
-                                        currency: "USD",
-                                    }
-                                )}
-                            </div>
-                        </div>
-                    )}
+                        )}
+                    </div>
+
+                    <CardButtons product={product} />
                 </div>
-
-                <CardButtons product={product} />
+                <div className="flex flex-row items-center justify-between w-full rounded-b-lg border-gold-star bg-indigo-400 px-4 py-2">
+                    <div className="text-lg font-medium text-white">
+                        Verified reviews
+                    </div>
+                    <div className="flex flex-row items-center space-x-3">
+                        <Star
+                            size={12}
+                            className="stroke-gold-star fill-gold-star"
+                        />
+                        <Star
+                            size={12}
+                            className="stroke-gold-star fill-gold-star"
+                        />
+                        <Star
+                            size={12}
+                            className="stroke-gold-star fill-gold-star"
+                        />
+                        <Star
+                            size={12}
+                            className="stroke-gold-star fill-gold-star"
+                        />
+                        <Star
+                            size={12}
+                            className="stroke-gold-star fill-gold-star"
+                        />
+                    </div>
+                </div>
             </motion.div>
         </AnimatePresence>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    fontFamily: {
-      sans: ['"ProximaNova"'],
-      serif: ['"ProximaNova"'],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        fontFamily: {
+            sans: ['"ProximaNova"'],
+            serif: ['"ProximaNova"'],
+        },
+        extend: {
+            colors: {
+                "loading-text": "#828282",
+                "loading-bg-light": "#EBEBEB",
+                "text-inactive": "#978D8D",
+                "text-active": "#111010",
+                "light-bg": "#F3F3F3",
+                "catalog-bg": "#e8e8e8",
+                "gold-star": "#FFD700",
+                "light-gold": "#FFED8A",
+            },
+            backgroundImage: {
+                "home-bg": "url('./img/background.png')",
+            },
+            transitionProperty: {
+                width: "width",
+            },
+        },
     },
-    extend: {
-      colors: {
-        "loading-text": "#828282",
-        "loading-bg-light": "#EBEBEB",
-        "text-inactive": "#978D8D",
-        "text-active": "#111010",
-        "light-bg": "#F3F3F3",
-        "catalog-bg": "#e8e8e8",
-      },
-      backgroundImage: {
-        "home-bg": "url('./img/background.png')",
-      },
-      transitionProperty: {
-        width: "width",
-      },
-    },
-  },
-  plugins: [],
+    plugins: [],
 };
