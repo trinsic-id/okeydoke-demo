5dff997411ba9349d3c54c4e9875031d1075d74d
Hersh Patel <hersh.patel@trinsic.id>
2022-11-10 08:52:20 -0500

    Improving filter button

--- src/layouts/Catalog/FilterButton.tsx
+++ src/layouts/Catalog/FilterButton.tsx
@@ -3,26 +3,30 @@ import { useRecoilState } from "recoil";
 import { filterProductsState } from "../../atoms/products";
 
 export const FilterButton = () => {
-  const [isFiltered, toggleFilter] = useRecoilState(filterProductsState);
-  return (
-    <Switch.Group>
-      <div className="flex flex-row items-center gap-2">
-        <Switch.Label className={`text-black text-lg`}>Filter</Switch.Label>
+    const [isFiltered, toggleFilter] = useRecoilState(filterProductsState);
+    return (
+        <Switch.Group>
+            <div className="flex flex-row items-center gap-2">
+                <Switch.Label className={`text-black text-lg`}>
+                    Filter
+                </Switch.Label>
 
-        <Switch
-          checked={isFiltered}
-          onChange={() => {
-            toggleFilter((val) => !val);
-          }}
-          className={`focus:outline-none bg-transparent border-2 border-black relative inline-flex items-center h-4 rounded-full w-8 transition ease-in-out duration-500`}
-        >
-          <span
-            className={`w-2 h-2 transform transition ease-in-out duration-300 bg-black rounded-full ${
-              isFiltered ? "translate-x-1" : "translate-x-4"
-            } ${!isFiltered && "opacity-30"}`}
-          />
-        </Switch>
-      </div>
-    </Switch.Group>
-  );
+                <Switch
+                    checked={isFiltered}
+                    onChange={() => {
+                        toggleFilter((val) => !val);
+                    }}
+                    className={`focus:outline-none bg-transparent border-2 border-black ${
+                        !isFiltered && "border-opacity-30"
+                    } relative inline-flex items-center h-4 rounded-full w-8 transition ease-in-out duration-500`}
+                >
+                    <span
+                        className={`w-2 h-2 transform transition ease-in-out duration-300 bg-black rounded-full ${
+                            isFiltered ? "translate-x-1" : "translate-x-4"
+                        }`}
+                    />
+                </Switch>
+            </div>
+        </Switch.Group>
+    );
 };
