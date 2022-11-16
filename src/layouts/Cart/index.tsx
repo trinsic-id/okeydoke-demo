f14510a943eb2f285ea93fb230a376826f9a19f4
Hersh Patel <hersh.patel@trinsic.id>
2022-11-16 12:16:14 -0800

    Animations

--- src/layouts/Cart/index.tsx
+++ src/layouts/Cart/index.tsx
@@ -1,9 +1,36 @@
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { cartState } from "../../atoms/cart";
 import { CartItem } from "./CartItem";
 import { Delivery } from "./Delivery";
+
+const Animations = {
+    container: {
+        visible: {
+            opacity: 1,
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.1,
+            },
+        },
+        hidden: {
+            opacity: 0,
+            transition: {
+                when: "afterChildren",
+            },
+        },
+    },
+    item: {
+        visible: { opacity: 1, x: 0 },
+        hidden: { opacity: 0, x: -100 },
+    },
+    filterText: {
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 },
+    },
+};
 
 export const Cart = () => {
     const cartItems = useRecoilValue(cartState);
@@ -17,14 +44,29 @@ export const Cart = () => {
         <div className="w-full h-full bg-catalog-bg p-4">
             {cartItems.length ? (
                 <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-                    <div className="flex flex-col h-min w-full items-start rounded-lg bg-white py-3 px-4 divide-y-2">
+                    <motion.div
+                        className="flex flex-col h-min w-full items-start rounded-lg bg-white py-3 px-4 divide-y-2"
+                        key="container"
+                        variants={Animations.container}
+                        initial="hidden"
+                        animate="visible"
+                        exit="hidden"
+                    >
                         <div className="w-full text-xl font-medium pb-1">
                             Cart
                         </div>
+
                         {cartItems.map((product, idx) => (
-                            <CartItem product={product} key={product.id} />
+                            <motion.div
+                                className="w-full"
+                                variants={Animations.item}
+                                key={product.id}
+                            >
+                                <CartItem product={product} key={product.id} />
+                            </motion.div>
                         ))}
-                    </div>
+                    </motion.div>
+
                     <Delivery />
                 </div>
             ) : null}
