870b451f121c250130e9ac395e17552013e1b868
Hersh Patel <hersh.patel@trinsic.id>
2023-04-18 12:37:45 -0400

    Fix

--- src/layouts/LandingPage/ActionList/Action.tsx
+++ src/layouts/LandingPage/ActionList/Action.tsx
@@ -15,12 +15,12 @@ export const Action = ({ text, image, onClick }: ActionProps) => {
             className=""
             onClick={onClick}
         >
-            <div className="group flex cursor-pointer flex-col items-center justify-between gap-12 rounded-lg border-2 bg-gray-50 p-5 align-baseline text-gray-900 transition-all duration-300 hover:border-blue-400 hover:p-8 hover:text-gray-600 hover:shadow-2xl">
+            <div className="group flex cursor-pointer flex-col items-center justify-between gap-12 rounded-lg border-2 bg-gray-50 p-5 align-baseline text-gray-900 transition-all duration-300 hover:border-blue-400 hover:p-8 hover:shadow-2xl">
                 <img
                     className="h-auto w-full max-w-md rounded-lg md:max-w-sm"
                     src={image}
                 />
-                <div className="text-xl transition-all duration-300 group-hover:text-blue-500">
+                <div className="text-xl transition-colors duration-300 group-hover:text-blue-500">
                     {text}
                 </div>
             </div>
