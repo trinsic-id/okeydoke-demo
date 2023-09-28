b20898306391c681c455988be01cb4f0972bc6f7
Tomislav Markovski <tmarkovski@gmail.com>
2023-09-28 09:02:15 -0400

    Use connect client

--- src/utils/generateSettings.ts
+++ src/utils/generateSettings.ts
@@ -1,11 +1,9 @@
 import {
-    defaultAuthSettings,
     defaultEcosystem,
     defaultSchema,
 } from "../services/AuthService";
 
 export const generateSettings = (ecosystem?: string, schema?: string) => ({
-    ...defaultAuthSettings,
     extraQueryParams: {
         "trinsic:ecosystem": ecosystem ? ecosystem : defaultEcosystem,
         "trinsic:schema": schema ? schema : defaultSchema,
